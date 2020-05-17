f64c80a55a3661c9ad022b0466b75db2b7bb3b37
Marcos-111 <escalzomd@gmail.com>
2020-05-17 22:56:48 +0200

    Ejercicios resueltos

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -17,130 +17,95 @@ __version__ = "1.1"
 def ej1():
     # Ejercicios de práctica con números
 
-    '''
-    Realice un calculadora, se ingresará por línea de comando dos números reales
-    y se deberá calcular todas las operaciones entre ellos:
-    - Suma
-    - Resta
-    - Multiplicación
-    - División
-    - Exponente/Potencia
-    
-    - Para todos los casos se debe imprimir en pantalla el resultado aclarando
-      la operación realizada en cada caso y con que números se ha realizado la operación
-      ej: La suma entre 4.2 y 6.5 es 10.7
+    numero_1 = 3.56
+    numero_2 = 11.33
 
-    '''
+    suma = numero_1+numero_2
+    print("La suma entre",numero_1,"y",numero_2,"es",suma)
+
+    resta = numero_1-numero_2
+    print("La resta entre",numero_1,"y",numero_2,"es",resta)
+
+    Multiplicación = numero_1*numero_2
+    print("La multiplicacion entre",numero_1,"y",numero_2,"es",Multiplicación)
+
+    division = numero_1/numero_2
+    print("La division entre",numero_1,"y",numero_2,"es",division)
+
+    Potencia = numero_1**numero_2
+    print("El numero",numero_1,"elevado a la",numero_2,"es",Potencia)
+
+
 
 def ej2():
     # Ejercicios de práctica numérica y cadenas
     
-    '''
-    Realice un programa que consulte por consola:
-    - El nombre completo de la persona
-    - El DNI de la persona
-    - La edad de la persona
-    - La altura de la persona
+    print("Ingrese su nombre completo: ")
+    nombre_completo = str(input())
 
-    Finalmente el programa debe imprimir dos líneas de texto por separado
-    - En una línea imprimir el nombre completo y el DNI, aclarando de que campo se trata cada uno
-            Ej: Nombre Completo: Nombre Apellido , DNI:35205070,
-    - En la segunda línea se debe imprimir el nombre completo, edad y altura de la persona
-      Nuevamente debe aclarar el campo de cada uno, para el que lo lea entienda de que se
-      está hablando.
+    print("Ingrese su DNI: ")
+    dni = int(input())
 
-    '''
+    print("Ingrese su edad: ")
+    edad = int(input())
+
+    print("Ingrese su altura: ")
+    altura = float(input())
+
+    print("Nombre completo:",nombre_completo,",","DNI:",dni)
+    print("Nombre completo:",nombre_completo,",","Edad:",edad,",","Altura:",altura)
+
 def ej3():
     # Ejercicios de práctica con cadenas
-
-    '''
-    Realice un programa que determine cual sería el apellido de una persona
-    si se ingresaran los dos nombres completos de sus padres.
-    Dicha persona deberá tener los apellidos de ambos padres
-
-    - Primero el programa debe consultar el nombre completo del padre_1
-    - Luego el programa debe consultar el nombre completo del padre_2
-    - Luego debe consultar el nombre del hijo/a
-    - Debe extraer los apellidos de los padres
-    - Luego formar el nombre completo del hijo/a utilizando los apellidos de sus padres
-      y el nombre ingresado de dicha persona
-    - Imprimir en pantalla el resultado
-
-    NOTA: Para extraer el apellido del nombre completo recomendamos usar el método "split"
-    Mostraremos un ejemplo:
     
-    direccion_completa = 'Monroe 2716'
-    calle, altura = direccion_completa.split(' ')
+    print("Nombre completo del padre:")
+    nombre_padre = str(input())
+    a,b = nombre_padre.split(" ")
+    
+    print("Nombre completo de la madre:")
+    nombre_madre = str(input())
+    c,d = nombre_madre.split(" ")
+    
+    print("Nombre hija/o:")
+    nombre_hijo = str(input())
 
-    Les dejo por su cuenta a que busquen un poco más acerca de este método que seguramente
-    utilizarán mucho de acá en adelante. Les dejamos un link con algunos ejemplos más:
-    https://www.pythonforbeginners.com/dictionary/python-split
 
-    Cualquier duda con el método split pueden consultarla por el campus
-        
-    '''
+    print(nombre_hijo,b,d)   
 
 def ej4():
     # Ejercicios de práctica con cadenas
 
-    '''
-    Realice un programa que determine si una persona_2 es pariente de la persona_1
-    Para facilitar el ejercicio solo ingresar un nombre y un apellido por persona
-    Ingresar dichos datos como Nombre Apellido, es decir, primero el nombre y luego
-    el apellido, separado por un espacio.
-    - El programa debe consultar primero el nombre completo de la persona_1
-    - Luego debe consultar el nombre completo de la persona_2
-    - Debe extraer el apellido de la persona_2
-    - Luego preguntar si apellido de la persona_2 está contenido en el nombre completo
-      de la persona_1
-    - En caso de contenerlo, son parientes
-    - Imprimir en pantalla el resultado
+    print("Nombre completo de la primera persona: ")
+    persona1 = str(input())
 
-    NOTA: Para extraer el apellido del nombre recomendamos usar el método "split"
-    Mostraremos un ejemplo:
+    print("Nombre completo de la segunda persona")
+    persona2 = str(input())
 
-    direccion_completa = 'Monroe 2716'
-    calle, altura = direccion_completa.split(' ')
+    c,d = persona2.split(" ")
 
-    Les dejo por su cuenta a que busquen un poco más acerca de este método que seguramente
-    utilizarán mucho de acá en adelante. Les dejamos un link con algunos ejemplos más:
-    https://www.pythonforbeginners.com/dictionary/python-split
-
-    Cualquier duda con el método split pueden consultarla por el campus
-    
-        
-    '''
+    parientes = d in persona1
+    print(persona2,"es pariente de",persona1,"?",parientes)
 
 def ej5():
     # Ejercicios de práctica con cadenas
        
-    '''
-    Realice un programa que reciba por consola su nombre completo
-    e imprima en pantalla su nombre en los siguientes formatos:
-    - Todas las letras en minúsculas
-    - Todas las letras en mayúsculas
-    - Solo la primera letra del nombre en mayúscula
-
-    NOTA: Para realizar este ejercicio deberá usar los siguientes métodos
-    de strings:
-    - lower
-    - upper
-    - capitalize
-
-    Puede buscar en internet como usar en Python estos métodos.
-    Les dejamos un el siguiente link que posee casos de uso de algunos de ellos:
-
-    Link de referencia:
-    https://www.geeksforgeeks.org/isupper-islower-lower-upper-python-applications/
-
-    Cualquier duda con estos métodos pueden consultarla por el campus
+    print("Escriba su nombre completo: ")
+    nombre_completo = str.lower(input())
+    print(nombre_completo)
     
+    print("Escriba su nombre completo: ")
+    nombre_completo = str.upper(input())
+    print(nombre_completo)
 
-    '''
+    print("Escriba su nombre completo: ")
+    nombre_completo = str(input()) 
+    x = nombre_completo.capitalize()  
+    print(x)
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    ej1()
+    #ej1()
     #ej2()
     #ej3()
     #ej4()
+    #ej5()
